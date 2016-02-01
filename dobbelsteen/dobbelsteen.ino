a3b5b6042bb84f0e021125749493e19f1abc0d6c
JM <git@yodium.nl>
2016-02-01 22:25:53 +0100

    added button

--- dobbelsteen/dobbelsteen.ino
+++ dobbelsteen/dobbelsteen.ino
@@ -1,21 +1,27 @@
 
-int poort1 = 2;
+int ledpoort1 = 2;
+int knopjepoort2 = 3;
 
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(115200);
-pinMode(poort1, OUTPUT);
+pinMode(ledpoort1, OUTPUT);
+pinMode(knopjepoort2, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+if (digitalRead(knopjepoort2) ==HIGH) {
+  Serial.println("hoi");
+}
 for(int i=1;i<6;i++){
 
-digitalWrite(poort1, HIGH);
+digitalWrite(ledpoort1, HIGH);
 Serial.println(i*1000);
 delay(i*1000);
-digitalWrite(poort1, LOW);
+digitalWrite(ledpoort1, LOW);
 delay(i*1000);
 }
 }
